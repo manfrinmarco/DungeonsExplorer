65f4a81009090d6183be51bcbaf4a5ed476b54b6
Marco_M <marco.manfrin@sistec-pn.it>
2025-05-22 15:22:03 +0200

    entityes refactor

--- src/main/java/manfrinmarco/entities/Enemy.java
+++ src/main/java/manfrinmarco/entities/Enemy.java
@@ -4,11 +4,21 @@ import manfrinmarco.items.Item;
 
 public class Enemy extends Entity {
     private static final long serialVersionUID = 1L;
+
     private final EnemyStrategy strategy;
+    private Item drop;
 
     public Enemy(String name, int health, EnemyStrategy strategy) {
         super(name, health);
         this.strategy = strategy;
+    }
+
+    public Item getDrop() {
+      return drop;
+    }
+
+    public void setDrop(Item drop) {
+        this.drop = drop;
     }
 
     public EnemyStrategy getStrategy() {
@@ -17,16 +27,6 @@ public class Enemy extends Entity {
 
     public void executeStrategy(Player player) {
         strategy.attack(this, player);
-    }
-
-    private Item drop;
-
-    public void setDrop(Item drop) {
-        this.drop = drop;
-    }
-
-    public Item getDrop() {
-      return drop;
     }
 }
     
